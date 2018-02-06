549e707b7292538214be0a1588a2937f7ce9c1e7
Denne Reed <denne.reed@gmail.com>
2018-02-06 14:41:43 -0800

    import_kmz

--- paleocore110/settings/__init__.py
+++ paleocore110/settings/__init__.py
@@ -1,2 +1,7 @@
 # flake8: noqa
-from .dev import *
+
+# In dev environment settngs is set in pycharm in Preferences > Languages & Frameworks > Django,
+# or from command line, e.g. python manage.py runserver --settings=paleocore110.settings.dev
+
+# wsgi points to settings generally, so this file only gets read by wsgi in the production environment
+from .production import *
