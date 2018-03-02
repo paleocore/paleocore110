fefc1a3dc5c4e3b69e816519a04777c5db4a38d6
Denne Reed <denne.reed@gmail.com>
2018-03-02 15:54:54 -0800

    gdb_initial

---
+++ gdb/admin.py
@@ -0,0 +1,131 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from .models import Occurrence, Biology, Locality
+import unicodecsv
+import os
+import projects.admin
+
+
+# Register your models here.
+class OccurrenceAdmin(admin.ModelAdmin):
+    list_display = ('specimen_number', 'item_scientific_name', 'item_description', 'locality',
+                    'date_collected', 'time_collected', 'date_time_collected', 'on_loan', 'date_last_modified')
+    list_filter = ['date_collected', 'on_loan', 'date_last_modified']
+
+    list_per_page = 1000
+
+
+class LocalityAdmin(projects.admin.PaleoCoreLocalityAdmin):
+    list_display = ('locality_number', 'locality_field_number', 'name', 'date_discovered',
+                    'point_x', 'point_y')
+    readonly_fields = ('locality_number', 'point_x', 'point_y', 'easting', 'northing', 'date_last_modified')
+    list_filter = ['date_discovered', 'formation', 'NALMA', 'region', 'county']
+    search_fields = ('locality_number', 'locality_field_number', 'name')
+
+
+class BiologyAdmin(admin.ModelAdmin):
+    list_display = ('specimen_number', 'item_scientific_name', 'item_description', 'locality',
+                    'date_collected', 'time_collected', 'date_time_collected', 'on_loan', 'date_last_modified')
+    list_filter = ['tax_order', 'family', 'genus', 'date_collected', 'on_loan', 'NALMA', 'date_last_modified',
+                   'locality']
+    list_per_page = 1000
+    search_fields = ['tax_class', 'tax_order', 'family', 'tribe', 'genus', 'specific_epithet', 'item_scientific_name',
+                     'specimen_number', 'cm_specimen_number', 'locality__locality_number', 'locality__name']
+    actions = ['create_data_csv', 'generate_specimen_labels']
+
+    def create_data_csv(self, request, queryset):
+        """
+        Export biology data to csv format. Still some issues with unicode characters.
+        :param request:
+        :param queryset:
+        :return:
+        """
+        response = HttpResponse(content_type='text/csv')  # declare the response type
+        # TODO generalize project file name
+        # Use ContentType.objects.get_for_model(myobject)
+        response['Content-Disposition'] = 'attachment; filename="GDB_Biology.csv"'  # declare the file name
+        writer = unicodecsv.writer(response)  # open a .csv writer
+        b = Biology()  # create an empty instance of a biology object
+
+        # Fetch model field names. We need to account for data originating from tables, relations and methods.
+        concrete_field_names = b.get_concrete_field_names()  # fetch a list of concrete field names
+        method_field_names = b.method_fields_to_export()  # fetch a list for method field names
+
+        fk_fields = [f for f in b._meta.get_fields() if f.is_relation]  # get a list of field objects
+        fk_field_names = [f.name for f in fk_fields]  # fetch a list of foreign key field names
+
+        # Concatenate to make a master field list
+        field_names = concrete_field_names + method_field_names + fk_field_names
+        writer.writerow(field_names)  # write column headers
+
+        def get_fk_values(occurrence, fk):
+            """
+            Get the values associated with a foreign key relation
+            :param occurrence:
+            :param fk:
+            :return:
+            """
+            qs = None
+            return_string = ''
+            try:
+                qs = [obj for obj in getattr(occurrence, fk).all()]  # if fk is one to many try getting all objects
+            except AttributeError:
+                return_string = str(getattr(occurrence, fk))  # if one2one or many2one get single related value
+
+            if qs:
+                try:
+                    # Getting the name of related objects requires calling the file or image object.
+                    # This solution may break if relation is neither file nor image.
+                    return_string = '|'.join([str(os.path.basename(p.image.name)) for p in qs])
+                except AttributeError:
+                    return_string = '|'.join([str(os.path.basename(p.file.name)) for p in qs])
+
+            return return_string
+
+        for occurrence in queryset:  # iterate through the occurrence instances selected in the admin
+            # The next line uses string comprehension to build a list of values for each field.
+            # All values are converted to strings.
+            concrete_values = [getattr(occurrence, field) for field in concrete_field_names]
+            # Create a list of values from method calls. Note the parenthesis after getattr in the list comprehension.
+            method_values = [getattr(occurrence, method)() for method in method_field_names]
+            # Create a list of values from related tables. One to many fields have related values concatenated in str.
+            fk_values = [get_fk_values(occurrence, fk) for fk in fk_field_names]
+
+            row_data = concrete_values + method_values + fk_values
+            cleaned_row_data = ['' if i in [None, False, 'None', 'False'] else i for i in row_data]  # Replace ''.
+            writer.writerow(cleaned_row_data)
+
+        return response
+
+    class Media:
+        js = ['admin/js/list_filter_collapse.js']
+
+    create_data_csv.short_description = 'Download Selected to .csv'
+
+    def generate_specimen_labels(self, request, queryset):
+        """
+        Export a text report with biology specimen data formatted for labels
+        :param queryset:
+        :param request:
+        :return:
+        """
+        content = ""
+        for b in queryset:
+            specimen_data = "GDB Project\n{catalog_number}  {sci_name}\n" \
+                            "{description}\nLocality {locality}\n" \
+                            "{nalma} {date_collected}\n\n".format(catalog_number=b.specimen_number,
+                                                       sci_name=b.item_scientific_name,
+                                                       description=b.item_description,
+                                                              locality=b.locality,
+                                                              nalma=b.NALMA,
+                                                              date_collected=b.date_collected)
+            content = content + specimen_data
+        response = HttpResponse(content, content_type='text/plain')  # declare the response type
+        response['Content-Disposition'] = 'attachment; filename="Specimens.txt"'  # declare the file name
+        return response
+    generate_specimen_labels.short_description = 'Specimen Labels'
+
+admin.site.register(Occurrence, OccurrenceAdmin)
+admin.site.register(Biology, BiologyAdmin)
+admin.site.register(Locality, LocalityAdmin)
+
---
+++ gdb/models.py
@@ -0,0 +1,188 @@
+from django.contrib.gis.db import models
+from .ontologies import BASIS_OF_RECORD_VOCABULARY, ITEM_TYPE_VOCABULARY, COLLECTING_METHOD_VOCABULARY, \
+    COLLECTOR_CHOICES, SIDE_VOCABULARY
+import projects.models
+
+
+
+class Occurrence(projects.models.PaleoCoreOccurrenceBaseClass):
+    specimen_number = models.AutoField(primary_key=True)  # NOT NULL
+
+    cm_specimen_number = models.IntegerField(null=True, blank=True)  # CM SPec #
+    locality = models.ForeignKey("Locality", to_field="locality_number", null=True, blank=True)
+    date_time_collected = models.DateTimeField(null=True, blank=True)
+    date_collected = models.DateField(null=True, blank=True, editable=True)
+    time_collected = models.CharField(null=True, blank=True, max_length=50)
+    #date_last_modified = models.DateTimeField("Date Last Modified", auto_now=True)
+    basis_of_record = models.CharField("Basis of Record", max_length=50, blank=False, null=False,
+                                       choices=BASIS_OF_RECORD_VOCABULARY)  # NOT NULL
+    item_type = models.CharField("Item Type", max_length=255, blank=True, null=True,
+                                 choices=ITEM_TYPE_VOCABULARY)
+    collecting_method = models.CharField("Collecting Method", max_length=50, blank=True, null=True,
+                                         choices=COLLECTING_METHOD_VOCABULARY, )
+    related_catalog_items = models.CharField("Related Catalog Items", max_length=50, null=True, blank=True)
+    item_scientific_name = models.CharField("Scientific Name", null=True, blank=True, max_length=255)  # Taxon
+    item_description = models.CharField("Description", null=True, blank=True, max_length=255)
+    image = models.FileField(max_length=255, blank=True, upload_to="uploads/images/gdb", null=True)
+
+    # Disposition fields
+    loan_date = models.DateField(null=True, blank=True)
+    loan_recipient = models.CharField(null=True, blank=True, max_length=255)
+    on_loan = models.BooleanField(default=False)  # Loan Status
+
+    # Geospatial
+    #geom = models.GeometryField(srid=4326, blank=True, null=True)
+    elevation = models.DecimalField(max_digits=10, decimal_places=4, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.specimen_number)
+
+    @staticmethod
+    def method_fields_to_export():
+        """
+        Method to store a list of fields that should be added to data exports.
+        Called by export admin actions.
+        These fields are defined in methods and are not concrete fields in the DB so have to be declared.
+        :return:
+        """
+        return []  # The GDB project has no method fields to export
+
+
+    class Meta:
+        verbose_name = "GDB Occurrence"
+
+
+class Biology(Occurrence):
+    # Taxonomy fields
+    kingdom = models.CharField("Kingdom", null=True, blank=True, max_length=50)
+    phylum = models.CharField("Phylum", null=True, blank=True, max_length=50)
+    tax_class = models.CharField("Class", null=True, blank=True, max_length=50)  # Class
+    tax_order = models.CharField("Order", null=True, blank=True, max_length=50)  # Order
+    family = models.CharField("Family", null=True, blank=True, max_length=50)
+    subfamily = models.CharField("Subfamily", null=True, blank=True, max_length=50)
+    tribe = models.CharField("Tribe", null=True, blank=True, max_length=50)
+    genus = models.CharField("Genus", null=True, blank=True, max_length=50)
+    specific_epithet = models.CharField("Species Name", null=True, blank=True, max_length=50)  # Species
+    infraspecific_epithet = models.CharField("Infraspecies", null=True, blank=True, max_length=50)
+    infraspecific_rank = models.CharField("Infraspecies rank", null=True, blank=True, max_length=50)
+    # Identification fields
+    author_year_of_scientific_name = models.CharField(null=True, blank=True, max_length=50)
+    nomenclatural_code = models.CharField(null=True, blank=True, max_length=50)
+    identification_qualifier = models.CharField(null=True, blank=True, max_length=50)
+    identified_by = models.CharField(null=True, blank=True, max_length=100)
+    date_identified = models.DateTimeField(null=True, blank=True)
+    type_status = models.CharField(null=True, blank=True, max_length=50)
+    # Description fields
+    sex = models.CharField(null=True, blank=True, max_length=50)
+    life_stage = models.CharField(null=True, blank=True, max_length=50)
+    preparations = models.CharField(null=True, blank=True, max_length=50)
+    morphobank_num = models.IntegerField(null=True, blank=True)
+    element = models.CharField(null=True, blank=True, max_length=50)
+    side = models.CharField(null=True, blank=True, max_length=50)
+    attributes = models.CharField(null=True, blank=True, max_length=50)
+    notes = models.TextField(null=True, blank=True, max_length=64000)
+    lower_tooth = models.CharField(null=True, blank=True, max_length=50)
+    upper_tooth = models.CharField(null=True, blank=True, max_length=50)
+    jaw = models.CharField(null=True, blank=True, max_length=50)
+    mandible = models.CharField(null=True, blank=True, max_length=50)
+    maxilla = models.CharField(null=True, blank=True, max_length=50)
+    teeth = models.CharField(null=True, blank=True, max_length=50)
+    cranial = models.CharField(null=True, blank=True, max_length=50)
+    miscellaneous = models.CharField(null=True, blank=True, max_length=50)
+    vertebral = models.CharField(null=True, blank=True, max_length=50)
+    forelimb = models.CharField(null=True, blank=True, max_length=50)
+    hindlimb = models.CharField(null=True, blank=True, max_length=50)
+    NALMA = models.CharField(null=True, blank=True, max_length=50)
+    sub_age = models.CharField(null=True, blank=True, max_length=50)  # Subage
+
+    class Meta:
+        verbose_name = "GDB Biology"
+        verbose_name_plural = "GDB Biology Items"
+
+
+class Locality(projects.models.PaleoCoreLocalityBaseClass):
+    locality_number = models.IntegerField(primary_key=True)  # NOT NULL
+    locality_field_number = models.CharField(null=True, blank=True, max_length=50)
+    #name = models.CharField(null=True, blank=True, max_length=50)  # Locality Name
+    date_discovered = models.DateField(null=True, blank=True)
+    #formation = models.CharField(null=True, blank=True, max_length=50)  # Formation
+    #member = models.CharField(null=True, blank=True, max_length=50)
+    NALMA = models.CharField(null=True, blank=True, max_length=50)
+    survey = models.CharField(null=True, blank=True, max_length=50)
+    quad_sheet = models.CharField(null=True, blank=True, max_length=50)
+    verbatim_latitude = models.CharField(null=True, blank=True, max_length=50)  # Latitude
+    verbatim_longitude = models.CharField(null=True, blank=True, max_length=50)  # Longitude
+    verbatim_utm = models.CharField(null=True, blank=True, max_length=50)  # UTM
+    verbatim_gps_coordinates = models.CharField(null=True, blank=True, max_length=50)  # GPS
+    verbatim_elevation = models.IntegerField(null=True, blank=True)  # Elevation
+    gps_date = models.DateField(null=True, blank=True, editable=True)
+    resource_area = models.CharField(null=True, blank=True, max_length=50)
+    notes = models.TextField(null=True, blank=True)
+    cm_locality_number = models.IntegerField(null=True, blank=True)  # CM Loc #
+    region = models.CharField(null=True, blank=True, max_length=50)
+    blm_district = models.CharField(null=True, blank=True, max_length=50)
+    county = models.CharField(null=True, blank=True, max_length=50)
+    image = models.FileField(max_length=255, blank=True, upload_to="uploads/images/gdb", null=True)
+    #geom = models.GeometryField(srid=4326, blank=True, null=True)
+    #date_last_modified = models.DateTimeField("Date Last Modified", auto_now=True)
+
+    def __str__(self):
+        """
+        This method returns the locality number and name if both exist, or a string with
+        just the locality number if there is no name.
+        """
+        if self.name:
+            return str(self.locality_number)+"-"+self.name
+        else:
+            return str(self.locality_number)
+
+    def update_geom_from_verbatim(self):
+        if self.verbatim_latitude is not None:
+            lat_dms = self.verbatim_latitude
+            lon_dms = self.verbatim_longitude
+            lat_string = lat_dms.split()
+            lon_string = lon_dms.split()
+            if len(lat_string) == 4 and len(lon_string) == 4:
+                lat_dd = (float(lat_string[2])/60+float(lat_string[1]))/60+float(lat_string[0])
+                if lat_string[3]=="S":
+                    lat_dd *= -1  # southern latitudes should be negative
+                lon_dd = (float(lon_string[2])/60+float(lon_string[1]))/60+float(lon_string[0])
+                if lon_string[3]=="W":
+                    lon_dd *= -1
+            return "POINT ("+str(lon_dd)+" "+str(lat_dd)+")"
+
+    def point_x(self):
+        try:
+            return self.geom.x
+        except:
+            return 0
+
+    def point_y(self):
+        try:
+            return self.geom.y
+        except:
+            return 0
+
+    def easting(self):
+        try:
+            utm_point = self.geom.transform(32637, clone=True)  # get a copy of the point in utm
+            return utm_point.x
+        except:
+            return 0
+
+    def northing(self):
+        try:
+            utm_point = self.geom.transform(32637, clone=True)
+            return utm_point.y
+        except:
+            return 0
+
+    class Meta:
+        verbose_name_plural = "GDB Localities"
+
+
+
+
+
+
+
