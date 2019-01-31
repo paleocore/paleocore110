d1258a590cae1d4b9686208e3df6dda888773453
dnr266 <reedd@austin.utexas.edu>
2019-01-31 09:53:48 -0800

    util.py updates

--- origins/util.py
+++ origins/util.py
@@ -236,3 +236,7 @@ def get_country_from_geom(geom):
         country_code = None
     return country_code
 
+
+state_abbr = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'VI', 'WA', 'WV', 'WI', 'WY']
+
+
