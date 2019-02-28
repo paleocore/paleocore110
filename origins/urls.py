7a5d11f53750b7d016e2b5fb5d67c97ef238ea67
dnr266 <reedd@austin.utexas.edu>
2019-02-27 16:06:02 -0800

    fixed comment indicating proper url form for geojson data

--- origins/urls.py
+++ origins/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import url
 from origins.models import SitePage
-from djgeojson.views import GeoJSONLayerView
 from .views import MyGeoJSONLayerView
 
 urlpatterns = [
-               # url to get a geojson representation of all Origins sites
-               # ex. /origins/sites.geojson
+    # url to get a geojson representation of all Origins sites
+    # ex. /origins/origins.geojson  Note no trailing slash!
     url(r'^origins.geojson$',
         MyGeoJSONLayerView.as_view(model=SitePage,
                                    crs=False,
-                                 properties=['title', 'slug', 'url_path'],
-                                 geometry_field='location'),
+                                   properties=['title', 'slug', 'url_path'],
+                                   geometry_field='location'),
         name='sites_geojson'),
 ]
