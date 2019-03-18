c310a846dff867e2fca1622a1c123dd858cdcc74
dnr266 <reedd@austin.utexas.edu>
2019-03-18 15:06:10 -0700

    csv export to file

--- lgrp/utilities.py
+++ lgrp/utilities.py
@@ -1,6 +1,8 @@
 from .models import *
 from difflib import SequenceMatcher
 from itertools import permutations
+import csv
+import time
 
 
 def match_taxon(biology_object):
@@ -87,3 +89,62 @@ def prune_species():
         if Biology.objects.filter(taxon=taxon).count() == 0 and taxon.rank == species:
             taxon.delete()
 
+
+def export():
+    start = time.time()
+    with open('lgrp_export_test.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        b = Biology()  # create an empty instance of a biology object
+        queryset = Biology.objects.all()
+
+        # Fetch model field names. We need to account for data originating from tables, relations and methods.
+        concrete_field_names = b.get_concrete_field_names()  # fetch a list of concrete field names
+        method_field_names = b.method_fields_to_export()  # fetch a list for method field names
+
+        fk_fields = [f for f in b._meta.get_fields() if f.is_relation]  # get a list of field objects
+        fk_field_names = [f.name for f in fk_fields]  # fetch a list of foreign key field names
+
+        # Concatenate to make a master field list
+        field_names = concrete_field_names + method_field_names + fk_field_names
+        writer.writerow(field_names)  # write column headers
+
+        def get_fk_values(occurrence, fk):
+            """
+            Get the values associated with a foreign key relation
+            :param occurrence:
+            :param fk:
+            :return:
+            """
+            qs = None
+            return_string = ''
+            try:
+                qs = [obj for obj in getattr(occurrence, fk).all()]  # if fk is one to many try getting all objects
+            except AttributeError:
+                return_string = str(getattr(occurrence, fk))  # if one2one or many2one get single related value
+
+            if qs:
+                try:
+                    # Getting the name of related objects requires calling the file or image object.
+                    # This solution may break if relation is neither file nor image.
+                    return_string = '|'.join([str(os.path.basename(p.image.name)) for p in qs])
+                except AttributeError:
+                    return_string = '|'.join([str(os.path.basename(p.file.name)) for p in qs])
+
+            return return_string
+
+        for occurrence in queryset:  # iterate through the occurrence instances selected in the admin
+            # The next line uses string comprehension to build a list of values for each field.
+            # All values are converted to strings.
+            concrete_values = [getattr(occurrence, field) for field in concrete_field_names]
+            # Create a list of values from method calls. Note the parenthesis after getattr in the list comprehension.
+            method_values = [getattr(occurrence, method)() for method in method_field_names]
+            # Create a list of values from related tables. One to many fields have related values concatenated in str.
+            fk_values = [get_fk_values(occurrence, fk) for fk in fk_field_names]
+
+            row_data = concrete_values + method_values + fk_values
+            cleaned_row_data = ['' if i in [None, False, 'None', 'False'] else i for i in row_data]  # Replace ''.
+            writer.writerow(cleaned_row_data)
+
+    csvfile.close()
+    end = time.time()
+    return print(end-start)
